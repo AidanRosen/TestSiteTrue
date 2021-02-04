b462d2aed16b12966c41c3c562f6700cebc93861
AidanRosen <aidanrosen04@gmail.com>
2021-02-04 08:17:49 -0800

    Adding new website

---
+++ src/test/java/com/example/testingsite/TestingsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.testingsite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestingsiteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
